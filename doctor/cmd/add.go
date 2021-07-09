c9f2f80a52b93d766a396753588954ac52985be2
kmaasrud <km@aasrud.com>
2021-07-09 11:26:16 +0200

    Rewrote add command

--- doctor/cmd/add.go
+++ doctor/cmd/add.go
@@ -8,35 +8,33 @@ import (
 	"path/filepath"
 	"strings"
 
-	"github.com/kmaasrud/doctor/core"
-	"github.com/kmaasrud/doctor/msg"
-	"github.com/kmaasrud/doctor/utils"
+	"github.com/kmaasrud/doctor"
+	"github.com/kmaasrud/doctor/log"
 )
 
 func Add(sectionName string, index int) error {
+    msg := log.Get()
 	var (
 		addIndex int
 		addTitle string
 		addPath  string
 	)
 
-	rootPath, err := utils.FindDoctorRoot()
+    doc, err := doctor.NewDocument()
 	if err != nil {
-		return errors.New("Could not add a new section. " + err.Error())
+		return err
 	}
 
-	secsDir := filepath.Join(rootPath, "secs")
-
 	// Find all existing sections
-	secs, err := utils.FindSections(rootPath)
-	if _, ok := err.(*utils.NoSectionsError); ok {
-		// NoSectionsError. secs directory might not exists, create it if not
-		if _, existErr := os.Stat(secsDir); os.IsNotExist(existErr) {
-			err := os.Mkdir(secsDir, 0755)
+	if len(doc.Chapters) < 1 {
+		// Source directory might not exists, create it if not
+        srcDir := doc.Config.Build.SourceDir
+		if _, existErr := os.Stat(srcDir); os.IsNotExist(existErr) {
+			err := os.Mkdir(srcDir, 0755)
 			if err != nil {
-				return errors.New("Could not create directory 'secs'. " + err.Error())
+				return fmt.Errorf("Could not create directory '%s'. %s", srcDir, err.Error())
 			}
-			msg.Info("Created directory 'secs'.")
+			msg.Info("Created directory '" + srcDir + "'.")
 		}
 	} else if err != nil {
 		return errors.New("Could not add a new section. " + err.Error())
@@ -46,16 +44,16 @@ func Add(sectionName string, index int) error {
 		// If index is specified, bump the index of all files above it by 1
 		addIndex = index
 		msg.Info("Reordering existing sections...")
-		for i := index; i < len(secs); i++ {
-			err := secs[i].ChangeIndex(i + 1)
+		for i := index; i < len(doc.Chapters); i++ {
+			err := doc.Chapters[i].ChangeIndex(i + 1)
 			if err != nil {
 				return errors.New("Could not bump index of existing section. " + err.Error())
 			}
 		}
 	} else {
 		// If no index is specified, insert the new section at the first non-occupied index
-		for i, sec := range secs {
-			if i < sec.Index {
+		for i, chapter := range doc.Chapters {
+			if i < chapter.Index {
 				break
 			}
 			addIndex += 1
@@ -66,12 +64,12 @@ func Add(sectionName string, index int) error {
 	addTitle = strings.ToUpper(string(sectionName[0])) + string(sectionName[1:])
 
 	// Paths consist of zero padded index, '_' and then the title, like this: '02_This is a section.md'
-	addPath = filepath.Join(rootPath, "secs", fmt.Sprintf("%02d", addIndex)+core.SectionSep+sectionName+".md")
+	addPath = filepath.Join(doc.Root, doc.Config.Build.SourceDir, fmt.Sprintf("%02d", addIndex)+doctor.ChapterSep+sectionName+".md")
 	err = ioutil.WriteFile(addPath, []byte("# "+addTitle+"\n\n"), 0666)
 	if err != nil {
 		return errors.New("Could not create new section. " + err.Error())
 	}
-	msg.Success(fmt.Sprintf("Created new section %s with index %d.", msg.Style(addTitle, "Bold"), addIndex))
+	msg.Success(fmt.Sprintf("Created new section \"%s\" with index %d.", addTitle, addIndex))
 
 	return nil
 }
